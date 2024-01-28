26da15ceec6c0f3171a6238ab1627e79d3605628
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-01-29 00:31:27 +0500

    Create task3.cpp

---
+++ Lab/01/task3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void findTarget(int size, int arr[], int target){
+    int flag=0;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            if(arr[i] + arr[j] == target && flag ==0){
+                cout<<arr[i]<<"+"<<arr[j]<<"="<<target<<endl;
+                cout<<"Because"<<" arr["<<i<<"]+"<<"arr["<<j<<"]="<<target<<" we return ["<<i<<","<<j<<"]"<<endl;
+                flag=1;
+            }
+        }
+    }
+}
+int main(){
+    cout<<"Enter size of array: ";
+    int size, target;
+    cin>>size;
+    int arr[size];
+    for(int i=0; i<size; i++){
+        cout<<"Enter element " << i+1 <<": ";
+        cin>>arr[i];
+    }
+    cout<<"Enter target: ";
+    cin>>target;
+    findTarget(size, arr, target);
+    return 0;
+}
