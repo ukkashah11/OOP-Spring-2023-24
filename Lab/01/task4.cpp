8d7e3692c688fee5f7532b30b53496378558f25b
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-01-29 12:07:11 +0000

    task 4 commit

---
+++ Lab/01/task4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int height[] = {1,8,6,2,5,4,8,3,7};
+    int i=0;
+    int distance;
+    int j=sizeof(height)/4 - 1;
+    int max = 0;
+    while(i<j){
+        if(j== i+1){
+            j=sizeof(height)/4 -1;
+            i++;
+        }
+        int base = j-i;
+        if(height[i]<height[j]){
+            distance = height[i];
+        }
+        if(height[i]>height[j]){
+            distance = height[j];
+        }
+        if(height[i] == height[j]){
+            distance = height[i];
+        }
+        if(max<(distance*base)){
+            max = distance*base;
+        }
+        j--;
+    }
+    cout<<"Max amount of water the container can hold is "<<max<<endl;
+    return 0;
+}
