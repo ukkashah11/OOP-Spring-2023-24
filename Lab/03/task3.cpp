ac9dd6c6ac7c73e337df714109eb3420d7e47985
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-15 09:02:20 +0000

    create task3.cpp

---
+++ Lab/03/task3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class Task{
+    private:
+    string details, date;
+    int month;
+    public:
+    void setDetails(string Details){
+        details = Details;
+    }
+    void setDate(string Date){
+        date = Date;
+    }
+    void setMonth(int Month)(
+        if(Month>0 && Month<=12){
+            month = Month;
+        }
+        else{
+            cout<<"Invalid input";
+        }
+    )
+    string getDetails(){
+        return details;
+    }
+    string getDate(){
+        return date;
+    }
+    int getMonth(){
+        return month;
+    }
+};
+class Calendar
+{
+private:
+    
+public:
+    Calendar(/* args */);
+    ~Calendar();
+};
+
+Calendar::task3(/* args */)
+{
+}
+
+Calendar::~task3()
+{
+}
+
+int main(){
+
+}
