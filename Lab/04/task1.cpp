f1549cf8f7aad73580f14eebf1869b5106c27c6f
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-15 10:27:06 +0000

    task1

--- Lab/04/task1.cpp
+++ Lab/04/task1.cpp
@@ -1,5 +1,77 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
+class Book{
+    private:
+    string name, author, ISBN;
+    int pages, read;
+    public:
+    bool isAllDigits(const std::string& str) {
+    for (char c : str) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+    }
+     void setName(string newName) {
+        name = newName;
+     }
     
+    void setAuthor(string newAuthor) {
+        author = newAuthor;
+    }
+    void setISBN(const string& newISBN) {
+        if(isAllDigits(newISBN) == true && newISBN.size() == 13){
+        ISBN = newISBN;
+        }
+        else{
+            cout<<"Invalid input"<<endl;
+        }
+    }
+    void setPages(int newPages) {
+        pages = newPages;
+    }
+    void setRead(int newRead) {
+        if(newRead<=getPages()){
+        read = newRead;
+        if(newRead==getPages()){
+           cout<<"Book finished"<<endl;
+        }
+        }
+        else{
+            cout<<"Invalid input"<<endl;
+        }
+    }
+    string getName() const {
+        return name;
+    }
+    string getAuthor() const {
+        return author;
+    }
+    string getISBN() const {
+        return ISBN;
+    }
+    int getPages() const {
+        return pages;
+    }
+    int getRead() const {
+        return read;
+    }
+   Book(string newName, string newAuthor, string newISBN, int newPages, int newRead){
+    setName(newName);
+    setAuthor(newAuthor);
+    setISBN(newISBN);
+    setPages(newPages);
+    setRead(newRead);
+   }    
+};
+int main(){
+    Book book("abc dhf", "ukkashah", "1234567890123", 200, 5);
+    cout << "Book Name: " << book.getName() << endl;
+    cout << "Author: " << book.getAuthor() << endl;
+    cout << "ISBN: " << book.getISBN() << endl;
+    cout << "Pages: " << book.getPages() << std::endl;
+    book.setRead(300); 
+    book.setRead(200);
 }
