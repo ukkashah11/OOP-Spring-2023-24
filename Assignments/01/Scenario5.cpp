837b592e2beee5dc77dd45c180ca4833e69a341e
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-25 19:24:33 +0000

    commit

--- Assignments/01/Scenario5.cpp
+++ Assignments/01/Scenario5.cpp
@@ -1,52 +1,185 @@
-#include<iostream>
+/*Syed Ukkashah Ahmed Shah
+  23K-0055
+  Added comments where necessary
+*/
+#include <iostream>
 #include <vector>
 using namespace std;
-const int now = 8;
-class Resteraunt{
-    private:
-    string resteraunt_name, location, resteraunt_code;
-    vector<string> menu_list,valid_coupon_codes_list;
+class Restaurant {
+public:
+    string restaurant_name, location;
+    vector<string> menu_list, valid_coupon_codes_list;
     vector<float> price_list;
-    vector<int>bill;
+    static int redeemed_coupons;
+    string restaurant_code;
+    vector<int> bill; // stores indices of purchased items
     float total;
-    static int coupons_redeemed_count;
-    public:
-    void display_menu(){
-        cout<<"\tMENU"<<endl;
-        for(int i=0;i<menu_list.size();i++){
-        cout<<"["<<i+1<<"]: "<<menu_list[i]<<" -> $"<<price_list[i]<<endl;
+    Restaurant(string name, string location, string code, vector<string> menu, vector<float> price) 
+    : restaurant_name(name), location(location), restaurant_code(code), menu_list(menu), price_list(price), total(0)  {}
+    void display_menu() {
+        for (int i = 0; i < menu_list.size(); i++)
+            cout << "[" << i << "]: " <<  menu_list[i] << " -> $" << price_list[i] << endl; 
+    }
+    void apply_discount() { //this function iterates through the bill list twice to see if item has been ordered more than once for the "Buy 1 Get One Free discount"
+        int count = 0;
+        for (int i : bill) {
+            for (int j : bill)
+                if (i == j) count++; 
+            if (count >= 2) { 
+                cout << "Discounted: $" << total - price_list[i] << endl; 
+                return;
+            }
         }
     }
     void generate_bill() {
         for (int i : bill) {
-            cout << "[" << i+1 << "]: "<<menu_list[i]<<" -> $"<<price_list[i]<<endl;
+            cout << "[" << i << "]: " <<  menu_list[i] << " -> $" << price_list[i] << endl;
             total += price_list[i];
         }
-        cout<<"Total: $"<<total<<endl;
-    }
-    void apply_discount(){
-
+        cout << "Total: $" << total << endl;
     }
 };
-class BOGOCoupon{
-    public:
-    string coupon_code, resteraunt_code;
+int Restaurant::redeemed_coupons = 0;
+class BOGOCoupon {
+public:
+    string coupon_code, restaurant_code;
     int valid_from, valid_until;
-    BOGOCoupon(string c_code, string res_code, int from, int until): coupon_code(c_code), resteraunt_code(res_code), valid_from(from), valid_until(until){}
-    bool is_valid(string res_code){
-        return (res_code != resteraunt_code && now>=valid_from && now<=valid_until) ? false : true;
+    BOGOCoupon(string c_code, string res_code, int from, int until)
+    : coupon_code(c_code), restaurant_code(res_code), valid_until(until), valid_from(from) {}
+    bool is_valid(string res_code, int curr_day) {
+        return (res_code != restaurant_code && curr_day >= valid_from && curr_day <= valid_until) ? false : true;
     }
 };
-class User{
-    public:
-    string name, mobile_num;
+class User {
+public:
+    string name, mobile_number;
     int age;
-    vector<BOGOCoupon> coupons_list, redeemed_coupons_list;
-    void Accumulate_coupon(BOGOCoupon coupon){
-        coupons_list.push_back(coupon);
-        cout<<"Coupon added to user list"<<endl;
+    vector<BOGOCoupon> coupons, redeemed_coupons;
+    User(string name, string phone, int age) : name(name), mobile_number(phone), age(age) {}
+    void accumulate_coupon(BOGOCoupon coupon) { coupons.push_back(coupon); }
+    bool is_redeemed(BOGOCoupon coupon) {
+        for (BOGOCoupon redeemed : redeemed_coupons)
+            if (coupon.coupon_code == redeemed.coupon_code) return true;
+        return false;
     }
-    // bool Has_valid_coupon(BOGOCoupon coupon){
-    //     coupon.is_valid()
-    // }
+    int has_valid_coupon(string res_code) {
+        for (int i = 0; i < coupons.size(); i++)
+            if (!is_redeemed(coupons[i]) && coupons[i].is_valid(res_code, 8)) return i;
+        return -1;
+    }
+
+    bool redeem_coupon(int coupon, string res_code) {
+        if (!is_redeemed(coupons[coupon]) && coupons[coupon].is_valid(res_code, 8)) {
+            redeemed_coupons.push_back(coupons[coupon]);
+            return true;
+        }
+
+        return false;
+    }
 };
+
+void menu(User u) {
+    cout << "Welcome"<<u.name<<":\n"
+         << "\t1. Menus\n"
+         << "\t2. Receipt\n"
+         << "\t3. Add Item\n"
+         << "\t4. Exit Menu\n"
+         << ">> ";
+}
+
+void get_receipt(User &user, Restaurant &res) {
+    int index = user.has_valid_coupon(res.restaurant_code);
+
+    cout << "Coupon available" << endl;
+    cout << user.coupons[index].restaurant_code << "-BOGO-" << user.coupons[index].coupon_code << endl;
+    cout << "Would you like to avail? (Y/N): ";
+    char choice;
+    cin >> choice;
+
+    if (choice == 'N' || choice == 'n') {
+        res.generate_bill();
+        return;
+    }
+
+    res.generate_bill();
+    res.apply_discount(); 
+    res.bill.clear(); //erasing all bill elements once receipt is generated & coupon is availed
+}
+
+User create_user() {
+    string name, phone;
+    int age;
+
+    cout << "Enter user name: ";
+    getline(cin, name);
+
+    cout << "Enter user phone number: ";
+    getline(cin, phone);
+
+    cout << "Enter user age: ";
+    cin >> age;
+
+    return { name, phone, age };
+}
+
+int main()
+{
+    cout<<"SYED UKKASHAH\n23K-0055\n\n"<<endl;
+    Restaurant r1("Food Haven", "City Center", "FH",
+        { "Sushi", "Pad Thai", "Mango Tango" },
+        { 8.99, 13.99, 9.99 }
+    );
+    Restaurant r2("Pixel Bites", "Cyber Street", "PB",
+        { "Binary Burger", "Quantum Quinoa", "Data Donuts" },
+        { 12.99, 6.99, 5.99 }
+    );
+    User user = create_user();
+    user.accumulate_coupon(BOGOCoupon("12345", "FH", 0, 30));
+    user.accumulate_coupon(BOGOCoupon("33367", "PB", 0, 30));
+    bool flag = true;
+    while(flag) {
+        int choice;
+        menu(user);
+        cin >> choice;
+        int res;
+        switch(choice) {
+        case 1:
+            cout << "Select restaurant\n"
+                 << "\t1. Food Haven\n"
+                 << "\t2. Pixel Bites\n"
+                 << ">> ";
+            cin >> res;
+            if (res == 1) r1.display_menu();
+            else if (res == 2) r2.display_menu();
+            break;
+        case 2:
+            cout << "Select restaurant\n"
+                 << "\t1. Food Haven\n"
+                 << "\t2. Pixel Bites\n"
+                 << ">> ";
+            cin >> res;
+            cout << "test\n";
+            if (res == 1) get_receipt(user, r1);
+            else if (res == 2) get_receipt(user, r2);
+            break;
+        case 3:
+            cout << "Select restaurant\n"
+                 << "\t1. Food Haven\n"
+                 << "\t2. Pixel Bites\n"
+                 << ">> ";
+            cin >> res;
+            int item;
+            cout << "Enter item number: ";
+            cin >> item;
+            if (res == 1) r1.bill.push_back(item);
+            else if (res == 2) r2.bill.push_back(item);
+            break;
+        case 4:
+            flag = false;
+            break;
+        default:
+            cout << "Invalid input" << endl;
+        }
+    }
+    return 0;
+}
