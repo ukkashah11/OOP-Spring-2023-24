2bde4fb62510dd7d475e790c3cbb24d545637ce6
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-25 14:26:34 +0000

    commit

---
+++ Assignments/01/Scenario3.cpp
@@ -0,0 +1,140 @@
+/*Syed Ukkashah Ahmed Shah
+  23K-0055
+  Added comments where necessary
+*/
+#include <iostream>
+#include <cctype>
+using namespace std;
+class ChessPiece{
+    private:
+    string name, color;
+    char symbol;
+    public:
+    void setName(string name){
+        this->name = name;
+    }
+    void setColor(string color){
+        this->color = color;
+    }
+    void setSymbol(){
+        if(color == "black"){
+            symbol = name[0];
+            if(name=="Knight"){
+                symbol = 'N';
+            }
+        }
+        else{
+            symbol = tolower(name[0]); //lowercase symbol if color is black
+            if(name=="Knight"){
+                symbol = 'n';
+            }
+        }
+    }
+    char getSymbol()const{
+        return symbol;
+    }
+    string getColor()const{
+        return color;
+    }
+    string getName()const{
+        return name;
+    }
+    ChessPiece(){
+        setName("Pawn");
+        setColor("white");
+        setSymbol();
+    }
+    ChessPiece(string name, string color): name(name), color(color){setSymbol();}
+};
+class ChessBoard{
+private:
+ChessPiece* board[8][8];
+public:
+ChessBoard() {
+		// init white pieces
+        board[0][0] = new ChessPiece("Rook", "white");
+        board[0][1] = new ChessPiece("Knight", "white");
+        board[0][2] = new ChessPiece("Bishop", "white");
+        board[0][3] = new ChessPiece("King", "white");
+        board[0][4] = new ChessPiece("Queen", "white");
+        board[0][5] = new ChessPiece("Bishop", "white");
+        board[0][6] = new ChessPiece("Knight", "white");
+        board[0][7] = new ChessPiece("Rook", "white");
+
+        // init white pawns
+        for (int i = 0; i < 8; i++) board[1][i] = new ChessPiece();
+
+        // init black pieces
+        board[7][0] = new ChessPiece("Rook", "black");
+        board[7][1] = new ChessPiece("Knight", "black");
+        board[7][2] = new ChessPiece("Bishop", "black");
+        board[7][3] = new ChessPiece("King", "black");
+        board[7][4] = new ChessPiece("Queen", "black");
+        board[7][5] = new ChessPiece("Bishop", "black");
+        board[7][6] = new ChessPiece("Knight", "black");
+        board[7][7] = new ChessPiece("Rook", "black");
+
+        // init black pawns
+        for (int i = 0; i < 8; i++) board[6][i] = new ChessPiece("Pawn", "black");
+
+        // empty slots
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = new ChessPiece(".", "none");
+            }
+        }
+	}
+void display() const 
+    {
+        cout << "  a b c d e f g h\n";
+        for(int i = 0; i < 8; ++i) {
+            cout << 8 - i << " ";
+            for(int j = 0; j < 8; ++j) {
+                if(board[i][j] == nullptr) {
+                    cout << ". ";
+                } else {
+                    cout << board[i][j]->getSymbol() << ' ';
+                }
+            }
+            cout << 8 - i << "\n";
+        }
+        cout << "  a b c d e f g h\n";
+    }
+    bool movePiece(string source, string destination) {
+		// getting all positions from string
+		int dest_row = 7-(destination[1]-'1'), dest_col = destination[0]-'a', src_row = 7-(source[1]-'1'), src_col = source[0]-'a';
+		char src_name = board[src_row][src_col]->getSymbol(), dest_name = board[dest_row][dest_col]->getSymbol(); // chess piece name
+		cout << "validating move for '" << src_name << "' from " << source << " to " << destination << ":" << endl;
+
+		if (src_name == 'n' || src_name == 'N') {
+			bool vertical = abs(dest_row - src_row) == 2 && abs(dest_col - src_col) == 1; // 2 up/down and 1 right/left
+			bool horizontal = abs(dest_row - src_row) == 1 && abs(dest_col - src_col) == 2; // 1 up/down and 2 right/left
+			// check for any of the cases and also make sure dest is empty
+			return vertical || horizontal && dest_name == '.';
+		} else if (src_name == 'p' || src_name == 'P') {
+			return src_col == dest_col && // checking if same column
+				// checking  upward  movement and obstacle
+				((src_row - dest_row == 1 || src_row - dest_row == 2) && board[src_row-1][src_col]->getSymbol() == '.') || 
+				// checking downward movement and obstacle
+				((dest_row - src_row == 1 || dest_row - src_row == 2) && board[src_row+1][src_col]->getSymbol() == '.');
+		}
+		return false;
+	}
+
+};
+int main(){
+    cout<<"SYED UKKASHAH\n23K-0055\n\n"<<endl;
+    ChessBoard board;
+	board.display();
+	cout << endl << endl;
+	// invalid move - knight
+	cout << (board.movePiece("b8", "c7") ? "valid" : "not valid") << endl << endl;
+	// valid move - knight
+	cout << (board.movePiece("b8", "c6") ? "valid" : "not valid") << endl << endl;
+	// invalid move - pawn
+	cout << (board.movePiece("b7", "b4") ? "valid" : "not valid") << endl << endl;
+	// valid move - pawn
+	cout << (board.movePiece("b7", "b5") ? "valid" : "not valid") << endl << endl;
+	return 0;
+    
+}
---
+++ Assignments/01/Scenario4.cpp
@@ -0,0 +1,145 @@
+#include<iostream>
+using namespace std;
+class RollerCoaster{
+    private:
+    string name;
+    float height, length, speed;
+    unsigned short capacity, current_riders;
+    bool ride_inprog;
+    public:
+    void setName(string name){
+        this->name=name;
+    }
+    void setHeight(float height){
+        this->height = height;
+    }
+    void setLength(float length){
+        this->length=length;
+    }
+    void setSpeed(float speed){
+        this->speed=speed;
+    }
+    void setCapacity(unsigned short capacity){
+        this->capacity=capacity;
+        if(capacity%2 != 0 || capacity%3 != 0){
+            capacity = (capacity + 1) / 2*2;
+            if(capacity<=3){
+                capacity+=3;
+            }
+        }
+    }
+    void setCurrentRiders(unsigned current_riders){
+        this->current_riders = current_riders;
+    }
+    void isRideInProg(bool ride_inprog){
+        this->ride_inprog = ride_inprog;
+    }
+    string getName()const{
+        return name;
+    }
+    float getHeight()const{
+        return height;
+    }
+    float getLength() const{
+        return length;
+    }
+    float getSpeed() const{
+        return speed;
+    }
+    unsigned short getCapacity() const{
+        return capacity;
+    }
+    unsigned short getCurrentRiders()const{
+        return current_riders;
+    }
+    bool isRideInProg() const{
+        return ride_inprog;
+    }
+    void stopRide(){
+        if(ride_inprog)
+        {
+            ride_inprog = false;
+            cout<<"Ride stopped"<<endl;
+        }
+        else{
+            cout<<"Ride is already is progress"<<endl;
+        }
+    }
+    int seatRiders(int passengers){
+        if(current_riders+passengers <= capacity && ride_inprog==false){
+            current_riders +=passengers;
+            cout<<passengers<<" Riders seated"<<endl;
+            return 0;
+        }
+        else{
+            return abs(capacity - (current_riders+passengers));
+        }
+    }
+    int startRide(){
+        if(ride_inprog)return -1;
+        if(current_riders == capacity){
+            ride_inprog = true;
+            cout<<"Ride started"<<endl;
+            return 1;
+        }
+        else{
+            return capacity - current_riders;
+        }
+    }
+   void unseatRiders(int passengers){
+    if(passengers>0 && passengers<=capacity && ride_inprog ==false){
+        current_riders -= passengers;
+    }
+   }
+   void accelerate(string roll) {
+    	for (auto it = roll.end()-1; it >= roll.begin(); it--) {
+    		if (*it == '0') continue;
+    		this->setSpeed(speed + (*it-'0'));
+    		return;
+    	}
+    }
+   void decelerate(string roll) {
+    	for (auto it = roll.begin(); it < roll.end(); it++) {
+    		if (*it == '0') continue;
+    		this->setSpeed(speed - (*it-'0'));
+    		return;
+    	}
+    }
+    RollerCoaster(string name, float height, float length, unsigned short capacity): name(name), height(height), length(length), capacity(capacity), ride_inprog(false){
+        if(capacity%2 != 0 || capacity%3 != 0){
+            capacity = (capacity + 1) / 2*2; //rounding off to closest multiple of 2
+            if(capacity<=3){
+                capacity+=3;
+            }
+        }
+    }
+    RollerCoaster(){
+        name = "roller coaster";
+        height = 500;
+        length = 2000;
+        capacity = 20;
+        ride_inprog = false;
+    }
+};
+int main(){
+    RollerCoaster rc1;
+    RollerCoaster rc2("roller coaster 2", 550, 1800, 15);
+    cout << rc2.getName() << " " << rc2.getCapacity() << endl;
+    rc2.startRide();
+	rc2.seatRiders(8);
+	rc2.stopRide();
+    cout << "Riders not seated: " << rc2.seatRiders(39) << endl;
+	cout << "Current riders: " << rc2.getCurrentRiders() << endl;
+	cout << "Not occupied seats: " << rc2.startRide() << endl;
+    rc2.accelerate("0055");
+	cout << "Speed: " << rc2.getSpeed() << endl;
+	for (int i = 0; i < 8; i++) rc2.decelerate("0055");
+	cout << "Speed: " << rc2.getSpeed() << endl;
+	rc2.stopRide();
+	cout << "Ride status: ";
+    if(rc2.isRideInProg()) cout<<" in progress"<<endl;
+    else cout<<" not in progress";
+	rc2.unseatRiders(3);
+	cout << "Current riders: " << rc2.getCurrentRiders() << endl;
+
+}
