8accb092987d19226ed5bb1d24d3c4946bcefa22
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-27 22:09:28 +0500

    Create inline.cpp

---
+++ Notes/inline.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+inline int cube(int s){ //inline funcs eliminate overhead and mae your program faster but overall size of program may increase
+  return s*s*s;
+}
+int main(){
+  cout<<"cube of 3 is "<<cube(3)<<endl; /*inlining is a request to compiler, not a command.
+                                          Inlining might be ignored if a func is recursive, contains loops, switch statements, contains static vars*/
+  return 0;                                        
+                                          
+}
