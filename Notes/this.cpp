0a20256ff91b8aadbd028e91460cf89b70e6e028
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-02-27 22:16:37 +0500

    Update this.cpp

--- Notes/this.cpp
+++ Notes/this.cpp
@@ -26,4 +26,7 @@ using namespace std;
   int main(){
     Test obj1;
     obj1.setX(10).setY(20); //setX return address of obj1, which is why setX.(setY) is possible
+    /*In C++, the this pointer is a constant pointer that holds the address of the current object, and you cannot change the address it points to.
+    The this pointer is meant to give you access to the members of the object on which a member function is called,
+    but it itself cannot be modified to point to a different object.*/
   }
