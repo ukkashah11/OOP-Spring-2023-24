e62c01421144883e31757654f10df28045a7a6a4
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2024-03-26 17:23:02 +0000

    commit

--- Assignments/02/task1.cpp
+++ Assignments/02/task1.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+class SecurityTool{
+    float cost;
+    int devices;
+    string securityLevel;
+    public:
+    SecurityTool(float c, int d, string sl):cost(c), devices(d), securityLevel(sl){}
+    void performScan(){
+        cout<<"Scanning for threats..."<<endl;
+    }
+};
