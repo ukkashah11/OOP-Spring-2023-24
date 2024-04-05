7571af8d0b93a3a8ce2ac768685010f9846a1533
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-04-05 23:15:14 +0500

    Update func_overriding.cpp

--- Notes/func_overriding.cpp
+++ Notes/func_overriding.cpp
@@ -26,6 +26,10 @@ int main(){
   ptr->hello(); //base class func is called since hello in derived is not overrided (virtual keyword not used)
   ptr->display(); /*derived class func is called since display has been overrided (virtual keyword used).
                     (DYNAMIC/RUN TIME POLYMORPHISM, LATE BINDING)*/
-                    
+  B* child_ptr;
+  child_ptr = (B*) ptr; /*we can explicitly cast the parent ptr that points to the address
+                          of the child class obj as a ptr of the child class type. This allows us to
+                          init it to a ptr of the child class type. This is known is downcasting*/
+  child_ptr->display();
   return 0;
 }
