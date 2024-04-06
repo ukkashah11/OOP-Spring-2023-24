24b8853cf209ff8ede0a3839d625a79e68af2e58
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-04-06 07:29:28 +0500

    Update func_overriding.cpp

--- Notes/func_overriding.cpp
+++ Notes/func_overriding.cpp
@@ -23,8 +23,8 @@ int main(){
   A* ptr; 
   B b;
   ptr = &b; //when a parent class ptr is assigned to the address of the derived class object, it is called upcasting
-  ptr->hello(); //base class func is called since hello in base class is not overwritten (virtual keyword not used)
-  ptr->display(); /*derived class func is called since display in base class has been overwritten (virtual keyword used).
+  ptr->hello(); //base class func is called since hello in base class is not overridden (virtual keyword not used)
+  ptr->display(); /*derived class func is called since display in base class has been overridden (virtual keyword used).
                     (DYNAMIC/RUN TIME POLYMORPHISM, LATE BINDING)*/
   B* child_ptr;
   child_ptr = (B*) ptr; /*we can explicitly cast the parent ptr that points to the address
